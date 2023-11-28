2ffb3e2cc8797f8a04d32aec5dde04517ded30fc
fikireguale <fikireguale@gmail.com>
2023-11-27 20:37:23 -0500

    initial

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -6,6 +6,9 @@ import numpy as np
 from copy import deepcopy
 import time
 
+import math
+import random
+
 
 @register_agent("student_agent")
 class StudentAgent(Agent):
@@ -23,6 +26,77 @@ class StudentAgent(Agent):
             "d": 2,
             "l": 3,
         }
+
+    class Node:
+        def __init__(self, state, parent=None):
+            self.state = state
+            self.parent = parent
+            self.children = []
+            self.wins = 0
+            self.visits = 0
+            self.untried_actions = self.get_untried_actions()
+
+        def get_untried_actions(self):
+            # Return a list of all possible actions from this state
+            pass
+
+        def is_terminal(self):
+            # Check if the state is a terminal state
+            pass
+
+        def rollout_policy(self):
+            # Define the policy for the rollout phase, typically random
+            pass
+
+        def expand(self):
+            # Expand the node by creating a new child node
+            pass
+
+        def best_child(self, c_param=1.41):
+            # Select the best child using the UCB1 formula
+            pass
+
+        def update(self, result):
+            # Update this node's data with the simulation result
+            pass
+    
+
+    def ucb1(parent, child, c_param=1.41):
+        """Calculate the UCB1 value for a child node."""
+        return (child.wins / child.visits) + c_param * math.sqrt(math.log(parent.visits) / child.visits)
+
+    def select_node(root):
+        """Select a node in the tree to perform a rollout."""
+        current_node = root
+        while not current_node.is_terminal():
+            if not current_node.untried_actions:
+                current_node = current_node.best_child()
+            else:
+                return current_node.expand()
+        return current_node
+
+    def rollout(node):
+        """Perform a rollout from the given node to the end of the game."""
+        current_state = node.state
+        while not current_state.is_terminal():
+            current_state = current_state.rollout_policy()
+        return current_state.get_result()
+
+    def backpropagate(node, result):
+        """Backpropagate the result of a rollout up the tree."""
+        while node is not None:
+            node.visits += 1
+            node.wins += result
+            node = node.parent
+
+    def monte_carlo_tree_search(root, iterations=1000):
+        """Perform the MCTS algorithm."""
+        for _ in range(iterations):
+            node = select_node(root)
+            result = rollout(node)
+            backpropagate(node, result)
+        
+        return max(root.children, key=lambda x: x.visits).state
 
     def step(self, chess_board, my_pos, adv_pos, max_step):
         """
